8286e319a67eeaef272889438ddc8c17bdb6cbb8
Betz <silverbez@gmail.com>
2021-12-27 12:31:17 +0200

    cleanup

--- app/src/mock/java/com/example/android/architecture/blueprints/todoapp/data/FakeTasksRemoteDataSource.kt
+++ app/src/mock/java/com/example/android/architecture/blueprints/todoapp/data/FakeTasksRemoteDataSource.kt
@@ -74,7 +74,7 @@ object FakeTasksRemoteDataSource : TasksDataSource {
     }
 
     override suspend fun completeTask(task: Task) {
-        val completedTask = Task(task.title, task.description, true, task.id, task.priority)
+        val completedTask = Task(task.title, task.description, true, task.id)
         TASKS_SERVICE_DATA[task.id] = completedTask
     }
 
@@ -83,7 +83,7 @@ object FakeTasksRemoteDataSource : TasksDataSource {
     }
 
     override suspend fun activateTask(task: Task) {
-        val activeTask = Task(task.title, task.description, false, task.id, task.priority)
+        val activeTask = Task(task.title, task.description, false, task.id)
         TASKS_SERVICE_DATA[task.id] = activeTask
     }
 
